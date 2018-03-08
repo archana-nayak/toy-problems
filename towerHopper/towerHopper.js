61fb73f3fff1a4dd41d48812b1b5eb2084ab2372
archana <archana.nayak@gmail.com>
2018-03-07 16:13:51 -0800

    another version

--- towerHopper/towerHopper.js
+++ towerHopper/towerHopper.js
@@ -32,7 +32,7 @@ var array = [3,2,1,0,4];//return false
 // var array = [2,3,1,1,4]; // return true.
 var canHop = towerHopper(array);
 console.log(canHop);
-
+//Bottom Up Approach without extra space
 // function canJump(nums) {
 //   var lastPos = nums.length - 1;
 //   for (var i = nums.length - 1; i >= 0; i--) {
@@ -42,3 +42,25 @@ console.log(canHop);
 //   }
 //   return lastPos === 0;
 // }
+
+//Bottom Up Approach with extra space(the above eliminates the need
+//for extra space)
+// "public boolean canJump(int[] nums) {
+//   Index[] memo = new Index[nums.length];
+//   for (int i = 0; i < memo.length; i++) {
+//       memo[i] = Index.UNKNOWN;
+//   }
+//   memo[memo.length - 1] = Index.GOOD;
+
+//   for (int i = nums.length - 2; i >= 0; i--) {
+//       int furthestJump = Math.min(i + nums[i], nums.length - 1);
+//       for (int j = i + 1; j <= furthestJump; j++) {
+//           if (memo[j] == Index.GOOD) {
+//               memo[i] = Index.GOOD;
+//               break;
+//           }
+//       }
+//   }
+
+//   return memo[0] == Index.GOOD;
+// }"
